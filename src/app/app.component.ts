a239a091e2676347f305cf6a246627b9760cba0d
katfastovets <tuuunes@gmail.com>
2018-05-30 22:18:21 +0300

    add angular cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-content',
+  templateUrl: './app.component.html',
+  styleUrls: ['./mainContent.scss']
+})
+export class AppComponent {
+  title = 'my app';
+}
