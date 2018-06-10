bf0822ecd034d16c097d4fbfb303eec8195984a5
katfastovets <tuuunes@gmail.com>
2018-06-10 17:31:05 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AsideComponent } from './aside/aside.component';
+import { UserComponent } from './user/user.component';
+import { SearchComponent } from './search/search.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BirthdaysComponent } from './birthdays/birthdays.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactGridComponent } from './contact-grid/contact-grid.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsideComponent
+    AsideComponent,
+    UserComponent,
+    SearchComponent,
+    CategoriesComponent,
+    BirthdaysComponent,
+    CreateContactComponent,
+    ContactFormComponent,
+    ContactGridComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
